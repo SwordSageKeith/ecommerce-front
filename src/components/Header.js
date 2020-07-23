d695985c37450b67aaa4e85ae26c3c18bd377d52
Keith Prince <keithcprince@gmail.com>
2020-07-23 16:15:55 -0400

    Start css styling. Im pushing this because a storm is picking up and I dont wanna lose work

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.css'
+
+function Header(){
+  return (
+    <header>
+      <h1>Temp Name</h1>
+      <h3>something</h3>
+      <h3>something</h3>
+      <h3>something</h3>
+    </header>
+  );
+
+}
+
+export default Header;
