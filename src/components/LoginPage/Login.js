73ecf900e97bf2df1d4e467301da6fec9d224c51
Keith Prince <keithcprince@gmail.com>
2020-08-12 23:25:22 -0400

    built login page

--- src/components/LoginPage/Login.js
+++ src/components/LoginPage/Login.js
@@ -2,23 +2,61 @@ import React from "react";
 import "./Login.css";
 
 class Login extends React.Component {
-
-  handleLogin() {
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      error: "",
+    };
   }
 
-  render () {
+  change = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    return <div className = "loginPage">
-      <h1>Log in with username or email</h1>
-      <form className = "loginform">
-        <label for = "loginInput"></label>
-        <input type = "text" placeholder = "email / username" id = "loginInput"></input>
-        <br/>
-        <label for = "passwordInput"></label>
-        <input type = "password" id = "passwordInput"></input> 
-      </form>
-    </div>
+  handleLogin = (e) => {
+    e.preventDefault();
+    alert ( "Username: " + this.state.username + "\n password: " + this.state.password);
+  }
+
+  render() {
+    return (
+      <div className="loginPage">
+        <h1>Log in with username or email</h1>
+        <form className="loginform" onSubmit = {this.handleLogin}>
+          <h3 name="error" value={this.state.error}></h3>
+          <label for="loginInput">
+            <input
+              type="text"
+              placeholder="email / username"
+              id="loginInput"
+              value={this.state.username}
+              name="username"
+              onChange={this.change}
+              required
+            ></input>
+          </label>
+
+          <br />
+          <label for="passwordInput">
+            <input
+              type="password"
+              id="passwordInput"
+              name="password"
+              value={this.state.password}
+              onChange={this.change}
+              required
+            ></input>
+          </label>
+
+          <br />
+          <input type="submit" />
+        </form>
+      </div>
+    );
   }
 }
 
